74547b40e2c1bf81eded923bb2cd42c632657325
17Takafumi <dieu.detruit@gmail.com>
2020-06-04 14:11:40 +0900

    bug fix in linear

--- include/grid/src/linear/arange.hpp
+++ include/grid/src/linear/arange.hpp
@@ -8,17 +8,11 @@ namespace Grid
 template <class T>
 std::vector<T> arange(T start, T stop, T step, bool endpoint = true)
 {
-    std::vector<T> result;
     std::size_t size = static_cast<std::size_t>((stop - start) / step) + (endpoint ? 1UL : 0UL);
-    result.reserve(size);
-    if (endpoint) {
-        for (; start <= stop; start += step) {
-            result.emplace_back(start);
-        }
-    } else {
-        for (; start < stop; start += step) {
-            result.emplace_back(start);
-        }
+    std::vector<T> result(size);
+    for (std::size_t i = 0; i < size; ++i) {
+        result[i] = start;
+        start += step;
     }
     return result;
 }
