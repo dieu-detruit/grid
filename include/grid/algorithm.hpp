6b30f6025c73ee0a4efef88ef37e4e66052d8225
17Takafumi <dieu.detruit@gmail.com>
2020-06-19 23:38:51 +0900

    add ifftshift

--- include/grid/algorithm.hpp
+++ include/grid/algorithm.hpp
@@ -1,3 +1,4 @@
 #pragma once
 
 #include <grid/src/algorithm/fftshift.hpp>
+#include <grid/src/algorithm/ifftshift.hpp>
--- include/grid/src/algorithm/ifftshift.hpp
+++ include/grid/src/algorithm/ifftshift.hpp
@@ -3,21 +3,21 @@
 #include <algorithm>
 #include <execution>
 #include <iterator>
+#include <vector>
 
 #include <grid/core.hpp>
 #include <grid/parallel.hpp>
-#include <strings.h>
-#include <vector>
+#include <grid/src/algorithm/fftshift.hpp>
 
 namespace Grid
 {
 
-// 1D fftshift
-// swap left half and right half
+// 1D ifftshift
+// inverse fftshift result
 template <typename value_type, typename measure_type, std::size_t N>
-void fftshift(Grid::GridArray<value_type, measure_type, N>& vec)
+void ifftshift(Grid::GridArray<value_type, measure_type, N>& vec)
 {
-    constexpr std::size_t left_size = (N % 2 == 0) ? N / 2 : N / 2 + 1;
+    constexpr std::size_t left_size = N / 2;
     if (to_parallelize) {
         std::rotate(std::execution::par_unseq, vec.begin(), vec.begin() + left_size, vec.end());
     } else {
@@ -26,10 +26,10 @@ void fftshift(Grid::GridArray<value_type, measure_type, N>& vec)
 }
 
 template <class grid_vector, std::enable_if_t<Impl::is_grid_vector_v<grid_vector> and Impl::get_rank_v<grid_vector> == 1, std::nullptr_t> = nullptr>
-void fftshift(grid_vector& vec)
+void ifftshift(grid_vector& vec)
 {
     std::size_t N = vec.range(0).N();
-    std::size_t left_size = (N % 2 == 0) ? N / 2 : N / 2 + 1;
+    std::size_t left_size = N / 2;
     if (to_parallelize) {
         std::rotate(std::execution::par_unseq, vec.begin(), vec.begin() + left_size, vec.end());
     } else {
@@ -37,7 +37,7 @@ void fftshift(grid_vector& vec)
     }
 }
 
-// 2D fftshift
+// 2D ifftshift
 //
 //  +---+---+      +---+---+
 //  | 2 | 1 |      | 4 | 3 |
@@ -45,58 +45,29 @@ void fftshift(grid_vector& vec)
 //  | 3 | 4 |      | 1 | 2 |
 //  +---+---+      +---+---+
 //
-//  if the number of rows / columns is odd, the center one is included in upper / left part
+//  inverse fftshift result
+//  if the number of rows / columns is odd, the center one is included in "lower / right" part
 //
-
 namespace Impl
 {
 template <class Iterator>
-inline void fftshift_impl(Iterator begin, Iterator end,
+inline void ifftshift_impl(Iterator begin, Iterator end,
     typename std::iterator_traits<Iterator>::difference_type N,
     typename std::iterator_traits<Iterator>::difference_type M)
 {
     using index_t = typename std::iterator_traits<Iterator>::difference_type;
 
     if (N % 2 == 0) {
-        // N = M = a even number -> N * N / 2 times
+        // N = M = a even number -> identical to fftshift
+        Impl::fftshift_impl(begin, end, N, M);
 
-        const index_t half = N / 2;
-
-        const index_t first_quadrant_offset = (N - 1) * half;
-        const index_t second_quadrant_offset = (N + 1) * half;
-
-        std::vector<Iterator> offset_list(half);
-        for (index_t i = 0; i < half; ++i) {
-            offset_list[i] = begin + N * i;
-        }
-        auto process = [&N, &first_quadrant_offset, &second_quadrant_offset, &half](auto& head) {
-            for (auto it = head; it < head + half; ++it) {
-                std::iter_swap(it, it + second_quadrant_offset);
-            }
-            for (auto it = head + half; it < head + N; ++it) {
-                std::iter_swap(it, it + first_quadrant_offset);
-            }
-        };
-        if (to_parallelize) {
-            std::for_each(std::execution::par_unseq, offset_list.begin(), offset_list.end(), process);
-        } else {
-            std::for_each(std::execution::seq, offset_list.begin(), offset_list.end(), process);
-        }
     } else {
-        // else -> (N - gcd(N - roundup(N/2)) * (M - gcd(M - roundup(M/2))
+        // else -> (N - gcd(N - rounddown(N/2)) * (M - gcd(M - rounddown(M/2))
         // Recursive Juggling Algorithm
         // Rotate lines and elements in each line in this order
-        //
-        //     A    B
-        //   +----+---+      +----+---+      +---+----+
-        // C | 2  | 1 |      | 3  | 4 |      | 4 | 3  |
-        //   |    |   |      +----+---+      +---+----+
-        //   +----+---|  ->  | 2  | 1 |  ->  | 1 | 2  |
-        // D | 3  | 4 |      |    |   |      |   |    |
-        //   +----+---+      +----+---+      +---+----+
 
-        const index_t B = N / 2, A = N - B;
-        const index_t D = M / 2, C = M - D;
+        const index_t A = N / 2, B = N - A;
+        const index_t C = M / 2, D = M - C;
 
         // Rotate lines
         const index_t g = std::gcd(N, A);
@@ -136,20 +107,20 @@ inline void fftshift_impl(Iterator begin, Iterator end,
 }  // namespace Impl
 
 template <typename value_type, typename measure_type, std::size_t N, std::size_t M>
-void fftshift(Grid::GridArray<value_type, measure_type, N, M>& vec)
+void ifftshift(Grid::GridArray<value_type, measure_type, N, M>& vec)
 {
-    Impl::fftshift_impl(vec.begin(), vec.end(), N, M);
+    Impl::ifftshift_impl(vec.begin(), vec.end(), N, M);
 }
 
 template <class grid_vector, std::enable_if_t<Impl::is_grid_vector_v<grid_vector> and Impl::get_rank_v<grid_vector> == 2, std::nullptr_t> = nullptr>
-void fftshift(grid_vector& vec)
+void ifftshift(grid_vector& vec)
 {
     using index_t = typename std::iterator_traits<decltype(vec.begin())>::difference_type;
 
     const index_t N = vec.range(0).N();
     const index_t M = vec.range(1).N();
 
-    Impl::fftshift_impl(vec.begin(), vec.end(), N, M);
+    Impl::ifftshift_impl(vec.begin(), vec.end(), N, M);
 }
 
 }  // namespace Grid
